5f88bf03b04ea1d2ffdefc5eef4411f524d57472
stereocat <stereocat@gmail.com>
2014-02-08 12:26:07 +0900

    add acl search function

--- lib/application.rb
+++ lib/application.rb
@@ -8,6 +8,8 @@ require 'stringio'
 require 'cisco_acl_intp'
 
 class CiscoAclWebApp < Sinatra::Base
+  set :bind, '0.0.0.0' # bind any addr when running WEBrick
+
   get '/' do
     redirect '/analyze'
   end
@@ -23,13 +25,31 @@ class CiscoAclWebApp < Sinatra::Base
     @parser.parse_file(StringIO.new(@raw_acls))
     @acl_table = @parser.acl_table
     @error_list = @parser.error_list
+    @search_opts = _gen_search_opts(params)
+    _puts_term @search_opts
     haml :analyze
   end
 
-  def _puts_term str
+  def _puts_term(str)
     puts '------------------------------'
     puts str
     puts '------------------------------'
+  end
+
+  def _default(value, default)
+    value == '' ? default : value
+  end
+  def _gen_search_opts(params)
+    opts = {}
+    opts[:protocol] = params[:protocol]
+    opts[:src_ip] = _default(params[:src_ip], '0.0.0.0')
+    opts[:dst_ip] = _default(params[:dst_ip], '0.0.0.0')
+
+    if ['tcp','udp'].include?(params[:protocol])
+      opts[:src_port] = _default(params[:src_port], 0)
+      opts[:dst_port] = _default(params[:dst_port], 0)
+    end
+    opts
   end
 end
 
